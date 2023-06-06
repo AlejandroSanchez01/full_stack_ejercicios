f62bd98d83eb17452e2dbc3931c4afc7ddc1af08
AlejandroSanchez <herlendy.sanchez11@gmail.com>
2023-06-05 20:16:30 -0500

    Actividad 10 Proyecto laravel

---
+++ Actividad_Final_10/my_project/database/migrations/2023_06_05_223836_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("name");
+            $table->string("last_name");
+            $table->string("id_type");
+            $table->integer("id_card");
+            $table->integer("phone");
+            $table->string("email");
+            $table->string("profession");
+            $table->string("role");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
