9d2a3a09c50721396aa94be1729297dbd9b6e5b6
AlejandroSanchez <herlendy.sanchez11@gmail.com>
2023-06-07 16:28:06 -0500

    Actividad 10 Proyecto laravel

--- Actividad_Final_10/my_project/routes/api.php
+++ Actividad_Final_10/my_project/routes/api.php
@@ -14,6 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get("/saludo",function (Request $request) {
+        $message = ["message" => "Hola mundo!"];
+
+        return response()->json($message);
+
+    });
+
+    Route::post("/usuario/crear",function (Request $request) {
+        $message = ["usuario" => "creado!"];
+
+        return response()->json($message);
+
+    });    
+
+    Route::get("/usuario/obtener",function (Request $request) {
+        $message = ["usuario" => "obtenido!"];
+
+        return response()->json($message);
+
+    }); 
+
+    Route::put("/usuario/actualizar",function (Request $request) {
+        $message = ["usuario" => "actualizado!"];
+
+        return response()->json($message);
+
+    }); 
+
+    Route::delete("/usuario/eliminar",function (Request $request) {
+        $message = ["usuario" => "eliminado!"];
+
+        return response()->json($message);
+
+    }); 
